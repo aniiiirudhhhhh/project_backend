99a35fc042811ea51a2e5fca954ae9113e5d4b68
aniiiirudhhhhh <spittala@loyaltymethods.com>
2025-09-10 12:21:49 +0530

    Delete rms_backend 3/rms_backend directory

--- rms_backend 3/rms_backend/server/routes/adminAuthRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const { registerAdmin, loginAdmin } = require("../controllers/adminAuthController");
-
-const router = express.Router();
-
-router.post("/admin/register", registerAdmin);
-router.post("/admin/login", loginAdmin);
-
-module.exports = router;
